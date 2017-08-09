3f298de3c654c1ee63d9e79df62cb946467611db
nik <supernoodz@users.noreply.github.com>
2017-08-09 19:09:46 +0100

    latest

--- automate/Custom/Integration/RedHat/CloudForms/Methods.class/__methods__/list_openstack_ems.rb
+++ automate/Custom/Integration/RedHat/CloudForms/Methods.class/__methods__/list_openstack_ems.rb
@@ -2,20 +2,14 @@
 # Description: <Method description here>
 #
 
-$evm.root.attributes.sort.each { |k, v| $evm.log(:info, "\t Attribute: #{k} = #{v}")}
-
 values_hash = {}
 values_hash['!'] = '-- select from list --'
 
-#ems = $evm.vmdb(:ExtManagementSystem).find_by(:type=>"ManageIQ::Providers::Openstack::CloudManager")
-#ems = $evm.vmdb(:ExtManagementSystem)
 ems = $evm.vmdb('ManageIQ_Providers_Openstack_CloudManager')
 
 $evm.log(:info, ems.count)
 
 ems.all.each do |e|
-  $evm.log(:info, e.name)
-  $evm.log(:info, e.hostname)
   values_hash[e.hostname] = e.name
 end
 
